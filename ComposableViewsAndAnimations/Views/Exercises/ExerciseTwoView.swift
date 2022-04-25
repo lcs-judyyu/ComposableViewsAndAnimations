85f2f3c24b5073377b04c64c8244a7fe161b669a
Judy <jyu@lcs.on.ca>
2022-04-24 21:27:18 -0400

    Finish exercise 2

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseTwoView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseTwoView.swift
@@ -14,21 +14,67 @@ struct ExerciseTwoView: View {
     
     // Controls whether this view is showing or not
     @Binding var showThisView: Bool
-        
+    
+    // Controls the size
+    @State private var scaleFactor: CGFloat = 1.0
+    
+    // Controls the hue
+    @State private var color: Color = .red
+    
+    // Controls the position
+    @State private var offset: CGFloat = 0
+    
     // Whether to apply the animation
     @State private var useAnimation = false
-
+    
+    @State private var expandOrShrink: CGFloat = 0.0
+    
     // MARK: Computed properties
-
+    
     var body: some View {
         
         NavigationView {
             
             VStack {
-
+                
                 Circle()
                     .frame(width: 200, height: 200)
-                    .foregroundColor(.blue)
+                    .foregroundColor(color)
+                    .scaleEffect(scaleFactor)
+                    .offset(x: 0, y: offset)
+                    .onTapGesture {
+                        // Change the color to a random color
+                        color = Color(hue: Double.random(in: 1...360) / 360.0,
+                                      saturation: 0.8,
+                                      brightness: 0.8)
+                        
+                        //animation change in position and size
+                        withAnimation(.easeIn(duration: 1)) {
+                            
+                            expandOrShrink += 0.1
+                            
+                            if expandOrShrink <= 1 {
+                                
+                                //expand and move up
+                                scaleFactor += 0.1
+                                offset += 20
+                                
+                            } else if expandOrShrink <= 2 {
+                                
+                                //shrink and move down
+                                scaleFactor -= 0.1
+                                offset -= 20
+                                
+                            } else {
+                                
+                                expandOrShrink = 0
+                                scaleFactor -= 0.1
+                                offset -= 20
+                            }
+                        }
+                    }
+                
+                Spacer()
                 
             }
             .navigationTitle("Exercise 2")
@@ -39,7 +85,7 @@ struct ExerciseTwoView: View {
                     }
                 }
             }
-
+            
         }
         
     }
