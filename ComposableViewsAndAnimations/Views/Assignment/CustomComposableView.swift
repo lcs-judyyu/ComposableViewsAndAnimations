6f906b69a15c36a0b7fab5317267cf81e20f6bcf
Judy <jyu@lcs.on.ca>
2022-04-26 22:44:04 -0400

    Create four circles that come close to the center and move away

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -12,18 +12,65 @@ struct CustomComposableView: View {
     //MARK: Stored Properties
     let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
     
+    //controls position
+    @State var topAndRightOffset: CGFloat = 100
+    @State var bottomAndLeftOffset: CGFloat = -100
+    
     //MARK: Computed Properties
     
     var body: some View {
-        ZStack {
-            Circle()
-        }
-        .animation(
-            Animation.easeInOut(duration: 2)
-                .repeatForever(autoreverses: true)
-        )
-        .onReceive(timer) { input in
-            timer.upstream.connect().cancel()
+        
+        VStack (spacing: 140) {
+            
+            ZStack {
+                
+                //top
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 60, height: 60)
+                    .offset(x: 0, y: topAndRightOffset)
+                    .opacity(0.7)
+
+                //right
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 60, height: 60)
+                    .offset(x: topAndRightOffset, y: 0)
+                    .opacity(0.7)
+
+                //bottom
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 60, height: 60)
+                    .offset(x: 0, y: bottomAndLeftOffset)
+                    .opacity(0.7)
+   
+                //left
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 60, height: 60)
+                    .offset(x: bottomAndLeftOffset, y: 0)
+                    .opacity(0.7)
+ 
+            }
+            .onReceive(timer) { input in
+                
+                withAnimation(
+                    Animation.easeInOut(duration: 0.5)
+                        .repeatForever(autoreverses: true)
+                ) {
+                    topAndRightOffset = 50
+                    bottomAndLeftOffset = -50
+                }
+                
+                //timer.upstream.connect().cancel()
+            }
+            
+            //text
+            Text("Loading...")
+                .foregroundColor(Color.gray)
+                .italic()
+                .bold()
         }
     }
 }
