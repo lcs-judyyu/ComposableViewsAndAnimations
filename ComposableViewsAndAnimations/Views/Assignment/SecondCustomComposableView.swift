26151b5fae314fcea9f677801da414c022d5be65
Judy <jyu@lcs.on.ca>
2022-04-28 18:59:52 -0400

    Replace the 9 rounded rectangles, use truncating remainder for the array (CGFloat)

--- ComposableViewsAndAnimations/Views/Assignment/SecondCustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/SecondCustomComposableView.swift
@@ -58,70 +58,11 @@ struct SecondCustomComposableView: View {
                     .frame(width: 310, height: outerHeight)
                 
                 //from left to right
-                Group {
                     RoundedRectangle(cornerRadius: 30, style: .continuous)
                         .frame(width: 70, height: outerHeight)
                         .foregroundColor(Color("aquamarine"))
                         .offset(x: 120)
                     
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color.white)
-                        .offset(x: 120 + firstInterval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color("aquamarine"))
-                        .offset(x: 120 + firstInterval + interval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color.white)
-                        .offset(x: 120 + firstInterval + 3 * interval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color("aquamarine"))
-                        .offset(x: 120 + firstInterval + 4 * interval)
-                }
-                
-                Group {
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color.white)
-                        .offset(x: 120 + firstInterval + 6 * interval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color("aquamarine"))
-                        .offset(x: 120 + firstInterval + 7 * interval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color.white)
-                        .offset(x: 120 + firstInterval + 9 * interval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color("aquamarine"))
-                        .offset(x: 120 + firstInterval + 10 * interval)
-                    
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .frame(width: 70, height: outerHeight)
-                        .foregroundColor(Color.white)
-                        .offset(x: 120 + firstInterval + 12 * interval)
-                }
-                
-                //end
-                RoundedRectangle(cornerRadius: 30, style: .continuous)
-                    .frame(width: 47, height: outerHeight)
-                    .foregroundColor(Color("aquamarine"))
-                    .offset(x: -133)
-                
-            }
-            
-            ZStack {
-                
                 ForEach([0.0,1.0,3.0,4.0,6.0,7.0,9.0,10.0,12.0], id: \.self) {
 
                     RoundedRectangle(cornerRadius: 30, style: .continuous)
@@ -129,6 +70,12 @@ struct SecondCustomComposableView: View {
                         .foregroundColor($0.truncatingRemainder(dividingBy: 3) == 0 ? Color.white : Color("aquamarine"))
                         .offset(x: 120 + firstInterval + $0 * interval)
                 }
+                
+                //end
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .frame(width: 47, height: outerHeight)
+                    .foregroundColor(Color("aquamarine"))
+                    .offset(x: -133)
                 
             }
             
